50ee7dad6b621381f523b21f494aef718814eade
bob <963206817@qq.com>
2017-11-21 18:08:14 +0800

    修改内容:     1.完成分支feature_rectangle的代码及注释

---
+++ sdk/rectangle.cpp
@@ -0,0 +1,15 @@
+#include "rectangle.hpp"
+
+using namespace SSDK;
+
+Rectangle::Rectangle()
+{
+    //初始化成员变量,将变量值赋为0
+    this->X = 0;
+    this->Y = 0;
+    this->angle = 0;
+    this->width = 0;
+    this->height = 0;
+}
+
+
