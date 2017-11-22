be0af52b190ddfa028b9351929c827d42e4c56e9
bob <963206817@qq.com>
2017-11-22 10:31:39 +0800

    修改内容:     1.完成feature_measureobj分支的代码及注释

--- job/measuredobj.cpp
+++ job/measuredobj.cpp
@@ -1,3 +1,15 @@
 #include "measuredobj.hpp"
 
+using namespace Job;
 
+MeasuredObj::MeasuredObj()
+{
+    this->m_pNextMeasuredObj = nullptr;
+    this->m_pPreMeasuredObj = nullptr;
+}
+
+MeasuredObj::~MeasuredObj()
+{
+    this->m_pNextMeasuredObj = nullptr;
+    this->m_pPreMeasuredObj = nullptr;
+}
